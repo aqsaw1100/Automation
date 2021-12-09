2e8529e42e81b75eb24826605c2628841d20abce
Aqsaw Handoo <aqsawh123@gmail.com>
2021-12-09 15:38:00 +0530

    commit 7

---
+++ cypress/integration/Login_WrongUserName_WrongPassword.js
@@ -0,0 +1,25 @@
+   ///<reference types="cypress" />
+
+///<reference types="cypress" />
+
+
+import { locators } from "d:/cypressQformautomation/cypress/Pages/locator"
+import { data, text} from  "d:/cypressQformautomation/cypress/Pages/function"
+import { info } from "d:/cypressQformautomation/cypress/Pages/userData"
+import { links } from "d:/cypressQformautomation/cypress/Pages/link"
+
+const call =new text()
+let url=links.link1
+let exlink=links.expectedlink
+
+it.only('InvaLidEmail',()=>{                           //randomly generated email is given thus incorrect email id
+    call.navigate(url);                                  //navigating q forms URL
+    call.remailAddress();                                           //calling function which generates random email address 
+    let rpassword=call.generateString(8);                               //calling function in which we are randomly generating password
+    cy.get(locators.password).type(rpassword)
+    call.onSubmit();
+    cy.url().should('include', exlink)       //asserting url as it will still contain url where we enter credentials 
+    call.errormessage()                      //capturing error message which we get once we try to log in using invalid password/email
+    cy.log("log in failed")                  //we are giving message that our test case has passed as our log in is failed
+    cy.screenshot()
+})
